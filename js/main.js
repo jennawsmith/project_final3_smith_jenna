fb4160046424f0518b16ca87386f6c9596591252
Jenna-Smith <jennawsmith@outlook.com>
2017-07-31 21:28:29 -0400

    add comments to main.js describing jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 
+//This is backstretch, jqeury number 1
 $(document).ready(function () {
     $.backstretch('img/brick.jpg');
 });
 
-
+//This calls my responsive tabs on menu page, jquery number 2
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
 });
