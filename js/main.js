883b2b7c495c6810ae2301b05f87f3170aee7475
Jenna-Smith <jennawsmith@outlook.com>
2017-07-31 22:11:11 -0400

    rearrange js file

--- js/main.js
+++ js/main.js
@@ -4,15 +4,16 @@ $(document).ready(function () {
     $.backstretch('img/brick.jpg');
 });
 
-//This is my javascript function that will add content
-
-document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
-
 
 //This is my jQuery function that will blur a specials image when clicking on it
 $('#specials').on('click', function () {
     $('.specialsimg').fadeIn(200);
 });
+
+//This is my javascript function that will add content
+
+document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
+
 
 //This calls my responsive tabs on menu page, jquery number 2
 $('#responsiveTabsDemo').responsiveTabs({
