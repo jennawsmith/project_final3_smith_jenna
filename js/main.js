48b36d64733bf8372c075ee8ad313253f66ec45d
Jenna-Smith <jennawsmith@outlook.com>
2017-07-31 22:04:28 -0400

    change jQuery comment description

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ $(document).ready(function () {
 document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
 
 
-//This is my jQuery function that adds hover effect
+//This is my jQuery function that adds an alert when clicking on an image in the special section
+
 
 //This calls my responsive tabs on menu page, jquery number 2
 $('#responsiveTabsDemo').responsiveTabs({
