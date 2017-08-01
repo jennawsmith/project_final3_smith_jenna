b134ac3ef046aac6dc396d0221fa0f511f0b2dde
Jenna-Smith <jennawsmith@outlook.com>
2017-07-31 21:51:20 -0400

    add empty div on contact page for js and add js innerHTML

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
     $.backstretch('img/brick.jpg');
 });
 
+//This is my javascript function that will add content
+
+document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
+
+
+//This is my jQuery function that adds hover effect
+
 //This calls my responsive tabs on menu page, jquery number 2
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
