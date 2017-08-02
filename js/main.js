6d016aaa4c66e808073403765cc9180e737eec23
Jenna-Smith <jennawsmith@outlook.com>
2017-08-02 19:58:33 -0400

    rearrange js file again

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
     $.backstretch('img/brick.jpg');
 });
 
+//This calls my responsive tabs on menu page, jquery number 2
+$('#responsiveTabsDemo').responsiveTabs({
+    startCollapsed: 'accordion'
+});
+
 //This is my jQuery function that will increase the size of a specials image when clicking on it
 $('.specialsimg').click(function () {
     $(this).css('width', '40%');
@@ -13,8 +18,3 @@ $('.specialsimg').click(function () {
 //THIS WON'T SHOW UP AFTER MY RESPONSIVE TABS ABOVE, but will show up if is placed above it... why?!
 
 document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
-
-//This calls my responsive tabs on menu page, jquery number 2
-$('#responsiveTabsDemo').responsiveTabs({
-    startCollapsed: 'accordion'
-});
