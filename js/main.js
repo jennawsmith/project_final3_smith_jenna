3ffbd69ddbbb212c4221d379d6e86af53c412bc6
Jenna-Smith <jennawsmith@outlook.com>
2017-08-01 21:44:17 -0400

    uncomment and rearrange scripts to troubleshoot errors

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ $('#specials').on('click', function () {
     $('.specialsimg').fadeIn(200);
 });
 
-//This is my javascript function that will add content
-
-document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
-
-
 //This calls my responsive tabs on menu page, jquery number 2
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
 });
+
+//This is my javascript function that will add content
+//THIS WON'T SHOW UP AFTER MY RESPONSIVE TABS ABOVE
+
+document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
