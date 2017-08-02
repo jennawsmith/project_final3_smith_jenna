77622802e86316d4bd69b46588b781cb6715abcf
Jenna-Smith <jennawsmith@outlook.com>
2017-08-02 18:57:43 -0400

    change jquery function to size increase

--- js/main.js
+++ js/main.js
@@ -4,18 +4,17 @@ $(document).ready(function () {
     $.backstretch('img/brick.jpg');
 });
 
-
-//This is my jQuery function that will blur a specials image when clicking on it
-$('#specials').on('click', function () {
-    $('.specialsimg').fadeIn(200);
-});
-
-//This calls my responsive tabs on menu page, jquery number 2
-$('#responsiveTabsDemo').responsiveTabs({
-    startCollapsed: 'accordion'
+//This is my jQuery function that will increase the size of a specials image when clicking on it
+$('.specialsimg').click(function () {
+    $(this).css('width', '40%');
 });
 
 //This is my javascript function that will add content
 //THIS WON'T SHOW UP AFTER MY RESPONSIVE TABS ABOVE, but will show up if is placed above it... why?!
 
 document.getElementById('review').innerHTML = '<h3>Please leave us a review!</h3>';
+
+//This calls my responsive tabs on menu page, jquery number 2
+$('#responsiveTabsDemo').responsiveTabs({
+    startCollapsed: 'accordion'
+});
